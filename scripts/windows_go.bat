ec3c3256e98829b28d8a9dedce76be0c29c9625d
Piorosen <aoikazto@gmail.com>
2022-02-02 20:15:15 +0900

    Update windows_go.bat

--- scripts/windows_go.bat
+++ scripts/windows_go.bat
@@ -4,4 +4,4 @@ if not exist .\gen\go\v1\ (
     mkdir .\gen\go\v1
 )
 
-protoc --go_out=.\gen\go\v1 --go_opt=paths=source_relative --go-grpc_out=.\gen\go\v1 --go-grpc_opt=paths=source_relative -I .\protos\v1 idl.proto
+for %%c in (.\protos\v1\*.proto) do protoc --go_out=.\gen\go\v1 --go_opt=paths=source_relative --go-grpc_out=.\gen\go\v1 --go-grpc_opt=paths=source_relative -I .\protos\v1 %%c
